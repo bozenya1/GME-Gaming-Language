3311c608711d435e266f6e7429c2d546bffc0db8
bozenya1 <strycharczykwojciech746@gmail.com>
2026-07-19 15:09:19 +0200

    Add files via upload

---
+++ GME-JS/src/Tokenizer.js
@@ -0,0 +1,70 @@
+const spec = [
+    //free space
+    [/^\s+/, null],
+
+    //commends
+    [/^\/\/.*/, null],
+    [/^\/\*[\s\S]*?\*\//, null],
+
+    //end
+    [/^;/, ';'],
+
+    // BlockStatement
+    [/^{/, '{'],
+    [/^}/, '}'],
+
+    //type of data
+    [/^\d+/, 'NUMBER'],
+    [/^"[^"]*"/, 'STRING'],
+    [/^'[^']*'/, 'STRING'], 
+
+    //BinaryExpression
+    [/^+/, '+'],
+    [/^-/, '-']
+];
+
+class Tokenizer{
+    init(string){
+        this._string = string;
+        this._cursor = 0;
+    }
+    isEOF(){
+        return this._cursor >= this._string.length;
+    }
+    hasMoreTokens(){
+        return this._cursor < this._string.length;
+    }
+    getNextToken(){
+        if(!this.hasMoreTokens()){
+            return null;
+        }
+        const string = this._string.slice(this._cursor);
+        
+        for (const [regexp, tokenType] of spec) {
+            const tokenValue = this._match(regexp, string);
+            if (tokenValue == null){
+                continue;
+            }
+            if(tokenType === null){
+                return this.getNextToken();
+            }
+            return {
+                type: tokenType,
+                value: tokenValue,
+            };
+        }
+        throw new SyntaxError(`Unexpected token: "${string[0]}"`);
+        return null;
+    }
+    _match(regexp, string){
+        const matched = regexp.exec(string);
+        if(matched === null){
+            return null;
+        }
+        this._cursor += matched[0].length;
+        return matched[0];
+    }
+}
+module.exports = {
+    Tokenizer,
+};
