c3648d58d5639366326802164d88922b7558694f
bozenya1 <strycharczykwojciech746@gmail.com>
2026-07-21 15:44:19 +0200

    Add files via upload

--- GME-JS/__tests__/math-test.js
+++ GME-JS/__tests__/math-test.js
@@ -1,52 +1,156 @@
 module.exports = test => {
-    test(`2+2;`, {
-        type: 'Program',
-        body: [
-                {
-                    type: 'ExpressionStatement',
-                    expression: {
-                        type: 'BinaryExpression',
-                        operator: '+',
-                        left: {
-                            type: 'NumericLiteral',
-                            value: '2',
-                        },
-                        right: {
-                            type: 'NumericLiteral',
-                            value: '2',
-                        },
-                    },
-                },
-            ],
+  test(`2 + 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '+',
+          left: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
         },
-    ),
-    test(`2+2-3;`, {
-        type: 'Program',
-        body: [
-                {
-                    type: 'ExpressionStatement',
-                    expression: {
-                        type: 'BinaryExpression',
-                        operator: '-',
-                        left: {
-                            type: 'BinaryExpression',
-                            operator: '+',
-                            left: {
-                                type: 'NumericLiteral',
-                                value: '3',
-                            },
-                            right: {
-                                type: 'NumericLiteral',
-                                value: '2',
-                            }
-                        },
-                        right: {
-                            type: 'NumericLiteral',
-                            value: '2',
-                        },
-                    },
-                },
-            ],
+      },
+    ],
+  });
+  test(`3 + 2 - 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '-',
+          left: {
+            type: 'BinaryExpression',
+            operator: '+',
+            left: {
+              type: 'NumericLiteral',
+              value: 3,
+            },
+            right: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
         },
-    );
+      },
+    ],
+  });
+
+  test(`2 * 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '*',
+          left: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+        },
+      },
+    ],
+  });
+
+  test(`2 * 2 * 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '*',
+          left: {
+            type: 'BinaryExpression',
+            operator: '*',
+            left: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+            right: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+        },
+      },
+    ],
+  });
+  test(`2 + 2 * 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '+',
+          left: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+          right: {
+            type: 'BinaryExpression',
+            operator: '*',
+            left: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+            right: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+          },
+        },
+      },
+    ],
+  });
+  test(`(2+2) * 2;`, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'BinaryExpression',
+          operator: '*',
+          left: {
+            type: 'BinaryExpression',
+            operator: '+',
+            left: {
+                type: 'NumericLiteral',
+                value: '2',
+            },
+            right: {
+                type: 'NumericLiteral',
+                value: '2',
+            },
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 2,
+          },
+        },
+      },
+    ],
+  });
 };
--- GME-JS/__tests__/run.js
+++ GME-JS/__tests__/run.js
@@ -14,24 +14,7 @@ const parser = new Parser();
 function exec(){
     const program = `
 
-        /**
-         * This is a comment multi line comment
-         */
-        "yara yara phonk tutorial";
-        {
-            {
-            42;
-            'hello princess'; //test in blocks
-                {
-                'mamy trzeci, czy bedzie 4?';
-                    {
-                    'mamy czwartyyyy!!!!';
-                    42; //numer dla sprawdzenia
-                    }
-                }
-            }
-        } //this is a single line comment
-        
+    (2 + 2) * 2;
     `;
 
     const ast = parser.parse(program);
